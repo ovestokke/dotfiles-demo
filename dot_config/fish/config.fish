c17cf0f391a92c76eb8357f745bbafe2199f2054
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2026-01-26 12:31:50 +0000

    Add .config/fish/config.fish

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,10 @@
+# Fish shell configuration
+#
+# This file runs AFTER all conf.d/*.fish files.
+# Configuration is modular - see conf.d/ for:
+#   00-env.fish          - Environment variables and paths
+#   10-tools.fish        - Tool initializations (oh-my-posh, zoxide)
+#   20-abbreviations.fish - Command abbreviations
+#   30-colors.fish       - Catppuccin Mocha theme
+
+# Add any machine-specific or experimental config here
